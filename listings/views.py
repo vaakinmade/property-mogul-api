cd735666d8814e7f3087e9c978c11204467ed8fc
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-18 00:36:25 +0100

    Added listing List and Create api view.

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from . import models
+from . import serializers
+
+#from rest_framework import generics
+
+class ListCreateListing(APIView):
+	def get(self, request, format=None):
+		listings = models.Listing.objects.all()
+		serializer = serializers.ListingSerializer(listings, many=True)
+		return Response(serializer.data)
+
