2e16149461f3dd40f05f919c34c62e6ea0d3a242
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-18 14:03:53 +0100

    wrote url routing for image CRUD views.

--- listings/urls.py
+++ listings/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
 	url(r'^$', views.ListCreateListing.as_view(), name="listing_list"),
 	url(r'^(?P<pk>\d+)/$', views.RetrieveUpdateDestroyListing.as_view(),
 		name='listing_detail'),
+	url(r'^images/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyImage.as_view(), name="image_view"),
+	url(r'^images/$', views.ListCreateImage.as_view(), name="image_list"),
 	
 ]
