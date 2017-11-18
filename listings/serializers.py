c92a5d15726c75c0a04d8a4256e30cb0212461af
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-18 14:06:55 +0100

    Image Serializer class

--- listings/serializers.py
+++ listings/serializers.py
@@ -6,7 +6,9 @@ from . import models
 class ListingSerializer(serializers.ModelSerializer):
 	class Meta:
 		extra_kwargs = {
-			'added_by': {'write_only': True}
+			'added_by': {'write_only': True},
+			'description': {'write_only': True},
+			'features': {'write_only': True}
 		}
 		fields = (
 			'name',
@@ -18,7 +20,36 @@ class ListingSerializer(serializers.ModelSerializer):
 			'bedroom',
 			'bathroom',
 			'added_by',
+		)
+		model = models.Listing
+
+
+class ListingDetailSerializer(serializers.ModelSerializer):
+	class Meta:
+		fields = (
+			'name',
+			'address',
+			'town',
+			'state',
+			'status',
+			'listing_type',
+			'bedroom',
+			'bathroom',
+			'description',
+			'features',
+			'added_by',
 			'created_at'
 		)
 		model = models.Listing
+
+
+class ImageSerializer(serializers.ModelSerializer):
+	class Meta:
+		fields = (
+			'image',
+			'listing',
+			'created_at'
+		)
+		model = models.Image
+
 
