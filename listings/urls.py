46b25a35ed6bf0cba544cbbcddcab2f3142d4bfa
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-18 02:46:38 +0100

    Added url routing for single listing retrieval, updating, and destruction.

--- listings/urls.py
+++ listings/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ListCreateListing.as_view(), name="listing_list"),
+	url(r'^(?P<pk>\d+)/$', views.RetrieveUpdateDestroyListing.as_view(),
+		name='listing_detail'),
 	
 ]
