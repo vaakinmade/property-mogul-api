281ba510f50be2ef19c1755c8ea7e43138938646
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-18 14:05:59 +0100

    New image model with ordering capabilities for listing model.

--- listings/models.py
+++ listings/models.py
@@ -11,7 +11,9 @@ class Listing(models.Model):
 	bedroom = models.IntegerField()
 	bathroom = models.IntegerField()
 	added_by = models.ForeignKey(User)
-	
+	description = models.TextField(default="")
+	features = models.TextField(default="")
+
 	LISTING_STATUS = (
 	    ('for sale','For Sale'),
 	    ('for rent', 'For Rent'),
@@ -33,3 +35,22 @@ class Listing(models.Model):
 	
 	def __str__(self):
 		return self.name
+
+
+class Image(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	listing = models.ForeignKey(Listing)
+	image = models.ImageField(upload_to='images/mogul', default='pic_folder/None/no-img.jpg')
+
+	ORDER = []
+	for i in range(1,7):
+		values = i, i
+		ORDER.append(values)
+	
+	ordering = models.IntegerField(
+        choices=ORDER,
+        default=0,
+    )
+
+	def __str__(self):
+		return str(self.listing) + " 's Image"
