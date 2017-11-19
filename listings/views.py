82c5065d8f72ba0a26c3a030bf1e734f3135d09b
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 03:07:16 +0100

    Wrote in SearchListView. Overode get_queryset and get_serializer methods.

--- listings/views.py
+++ listings/views.py
@@ -1,10 +1,12 @@
+from django.db.models import Q
+
 from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from . import models
 from . import serializers
 
-#from rest_framework import generics
 
 class ListCreateListing(generics.ListCreateAPIView):
 	queryset = models.Listing.objects.all()
@@ -24,4 +26,32 @@ class RetrieveUpdateDestroyImage(generics.RetrieveUpdateDestroyAPIView):
 class ListCreateImage(generics.ListCreateAPIView):
 	queryset = models.Image.objects.all()
 	serializer_class = serializers.ImageSerializer
+
+
+class SearchListView(generics.ListAPIView):
+
+	def get_queryset(self):
+		location = self.request.GET.get('location', None)
+		listing_type = self.request.GET.get('listing_type', None)
+		bedroom = self.request.GET.get('bedroom', None)
+		bathroom = self.request.GET.get('bathroom', None)
+		status = self.request.GET.get('status', None)
+		print ("location", location)
+
+		queryset = models.Listing.objects.filter(Q(town=location,
+													listing_type=listing_type,
+													bedroom=bedroom,
+													bathroom=bathroom) |
+												Q(state=location,
+													listing_type=listing_type,
+													bedroom=bedroom,
+													bathroom=bathroom)
+												)
+		return queryset
+
+	def get_serializer_class(self):
+		response = serializers.serialize("json", self.get_queryset())
+		return Response(response)
+
+
 
