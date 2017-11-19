56df682a6f0bde4072a08e60edf0d794ba230ae1
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 04:10:00 +0100

    reverted trailing slash removal

--- mogul/urls.py
+++ mogul/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls',
     							namespace='rest_framework')),
-    url(r'^api/v1/listings', include('listings.urls', namespace='listings')),
+    url(r'^api/v1/listings/', include('listings.urls', namespace='listings')),
 ]
