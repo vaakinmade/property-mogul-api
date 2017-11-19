88944c19b0235ad5447f1500ab7dcf613621a6f4
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-19 04:00:10 +0100

    removed trailing slashes from endpoints

--- listings/urls.py
+++ listings/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.ListCreateListing.as_view(), name="listing_list"),
-	url(r'^(?P<pk>\d+)/$', views.RetrieveUpdateDestroyListing.as_view(),
+	url(r'^(?P<pk>\d+)$', views.RetrieveUpdateDestroyListing.as_view(),
 		name='listing_detail'),
-	url(r'^images/(?P<pk>\d+)/$', views.RetrieveUpdateDestroyImage.as_view(), name="image_view"),
-	url(r'^images/$', views.ListCreateImage.as_view(), name="image_list"),
-	url(r'^search/$', views.SearchListView.as_view(), name='search'),	
+	url(r'^images$', views.ListCreateImage.as_view(), name="image_list"),
+	url(r'^images/(?P<pk>\d+)$', views.RetrieveUpdateDestroyImage.as_view(), name="image_view"),
+	url(r'^search$', views.SearchListView.as_view(), name='search'),	
 ]
