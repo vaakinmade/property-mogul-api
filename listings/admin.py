15cd43260b33b56c602597dec6cb649e44d5a61c
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-22 16:59:43 +0100

    corresponding adjustments for model field change

--- listings/admin.py
+++ listings/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Listing, Image
+from .models import Listing, ListingImage
 
 
 admin.site.register(Listing)
-admin.site.register(Image)
+admin.site.register(ListingImage)
 
