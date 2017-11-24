8b2db8a66726f715c5d2659e60561e4f8e7e80bd
vaakinmade <vickeyakinmade22@gmail.com>
2017-11-24 23:19:43 +0100

    Added url for sale listings view

--- listings/urls.py
+++ listings/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
 	url(r'^images$', views.ListCreateImage.as_view(), name="image_list"),
 	url(r'^images/(?P<pk>\d+)$', views.RetrieveUpdateDestroyImage.as_view(), name="image_view"),
 	url(r'^search$', views.SearchListView.as_view(), name='search'),
-	url(r'^rent$', views.RentListView.as_view(), name='rent'),	
+	url(r'^rent$', views.RentListView.as_view(), name='rent'),
+	url(r'^sale$', views.SaleListView.as_view(), name='sale'),
 ]
